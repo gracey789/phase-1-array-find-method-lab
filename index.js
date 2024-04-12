d057062d07226821a3da9c1bab95eec195e5e4c8
Gracey789 <Gmwende487@gmail.com>
2024-04-12 19:49:16 +0300

    feat: modified index.js

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // code your solution here
+superbowlWin = (record) => {
+    // Find the first record with a result of "W" (indicating a win)
+    const result = record.find( record => record.result === "W" );
+    
+    // If a win record is found, return the year of the win
+    return !!result ? result.year : undefined;
+  }
